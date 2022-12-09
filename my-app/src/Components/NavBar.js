eac32dd2d2ee2efdb7b880d0eb41880b86a34642
Jordyn <jordyn@example.com>
2022-12-09 12:08:04 -0500

    navbar links appear on the screen

---
+++ my-app/src/Components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function NavBar () {
+    return (
+        <nav className="NavBar">
+        <ul>
+            <li><a href="new">New</a></li>
+            <li><a href="makeup">Makeup</a></li>
+            <li><a href="skincare">Skincare</a></li>
+            <li><a href="bb">Bath & Body</a></li>
+        </ul>
+
+
+
+    </nav>
+    )
+}
